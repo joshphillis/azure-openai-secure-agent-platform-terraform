7486fc4769d7847c835e3b618108729d9ba16ab4
joshphillis <joshphillis@hotmail.com>
2026-03-11 12:38:51 -0500

    Refactor tags in resource group definition
    Merged existing tags with additional tags from var.tags.

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,29 +1,12 @@
-variable "project_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "environment" {
-  type = string
-}
-
 resource "azurerm_resource_group" "this" {
   name     = "${var.project_name}-${var.environment}-rg"
   location = var.location
 
-  tags = {
-    project     = var.project_name
-    environment = var.environment
-  }
-}
-
-output "name" {
-  value = azurerm_resource_group.this.name
-}
-
-output "id" {
-  value = azurerm_resource_group.this.id
+  tags = merge(
+    {
+      project     = var.project_name
+      environment = var.environment
+    },
+    var.tags
+  )
 }
