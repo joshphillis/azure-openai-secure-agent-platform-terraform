14ca78bd260bb74acb21ee19e90155470961e80e
joshphillis <joshphillis@hotmail.com>
2026-03-11 12:48:53 -0500

    Add Terraform configuration for container app environment

---
+++ modules/container_apps_env/main.tf
@@ -0,0 +1,18 @@
+locals {
+  name = "${var.project_name}-${var.environment}-cae"
+}
+
+resource "azurerm_container_app_environment" "this" {
+  name                = local.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  log_analytics_workspace_id = var.log_analytics_id
+
+  infrastructure_subnet_id = var.delegated_subnet_id
+
+  tags = {
+    project     = var.project_name
+    environment = var.environment
+  }
+}
