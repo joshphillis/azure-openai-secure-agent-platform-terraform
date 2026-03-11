2a4a1a8d24c3adee1f8c5c87d828927849b9f594
joshphillis <joshphillis@hotmail.com>
2026-03-11 12:42:22 -0500

    Add Log Analytics workspace module

---
+++ modules/log_analytics/main.tf
@@ -0,0 +1,18 @@
+locals {
+  name = var.workspace_name != null ?
+    var.workspace_name :
+    "${var.project_name}-${var.environment}-law"
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = local.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = var.retention_in_days
+
+  tags = {
+    project     = var.project_name
+    environment = var.environment
+  }
+}
